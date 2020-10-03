20121995b4464eca147a2c0f87f10cfb0688f5f1
kim eung gil <yesroad.dev@gmail.com>
2020-10-03 21:27:30 +0900

    mongoDB 연결

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
+require('dotenv').config();
+
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
+const mongoose = require('mongoose');
+
+// 비구조화 할당을 통해 process.env 내부 값에 레퍼런스 생성
+const { PORT, MONGO_URI } = process.env;
+
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false })
+  .then(() => {
+    console.log('Connected to MongoDb');
+  })
+  .catch((e) => {
+    console.error(e);
+  });
 
 const api = require('./api');
 
@@ -15,6 +30,8 @@ app.use(bodyParser());
 
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(4000, () => {
-  console.log('Listening to port 4000');
+// PORT가 지정되어 있지 않다면 4000을 사용
+const port = PORT || 4000;
+app.listen(port, () => {
+  console.log('Listening to port %d', port);
 });
