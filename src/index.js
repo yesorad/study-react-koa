0e7a41f06d7dc46892b280a6ddf1715121ae205a
kim eung gil <yesroad.dev@gmail.com>
2020-10-03 20:37:56 +0900

    posts route 적용

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 const Koa = require('koa');
+const Router = require('koa-router');
+
+const api = require('./api');
 
 const app = new Koa();
+const router = new Router();
+
+// 라우터 설정
+router.use('/api', api.routes()); // api 라우트 적용
+
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(4000, () => {
   console.log('Listening to port 4000');
