50535aeb00c8c20aa88d99f241e8658e97e2415e
kim eung gil <yesroad.dev@gmail.com>
2020-10-04 19:25:57 +0900

    react-router 설정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
+import PostListPage from './pages/posts/PostListPage';
+import LoginPage from './pages/auth/LoginPage';
+import RegisterPage from './pages/auth/RegisterPage';
+import PostPage from './pages/posts/PostPage';
+import WritePage from './pages/posts/WritePage';
 
 function App() {
-	return <div className='App'>123</div>;
+  return (
+    <>
+      <Route path={['/@:username', '/']} component={PostListPage} exact />
+      <Route path="/login" component={LoginPage} />
+      <Route path="/register" component={RegisterPage} />
+      <Route path="/write" component={WritePage} />
+      <Route path="/@:username/:postId" component={PostPage} />
+    </>
+  );
 }
 
 export default App;
